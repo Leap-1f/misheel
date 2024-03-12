7e505db962f599bd06e718e801a24110f9c55644
Misheel burenzul <23LP7177@G304-CC0083.local>
2024-03-12 19:18:08 +0800

    3/12

--- frontend/src/pages/signup.jsx
+++ frontend/src/pages/signup.jsx
@@ -1,20 +1,25 @@
 import { geld } from "../components/ui/img";
 import { useRouter } from "next/router";
 import { useState } from "react";
+import uuidv4 from "uuid";
 
 
 export default function SignUp (){
   const [name, setName] = useState();
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
+  const [repassword, setRepassword] = useState();
   const router = useRouter();
   const handleClick = () => {
     router.push("http://localhost:3000/")
   }
   const createData = async () => {
-    if (name === undefined || email === undefined || password === undefined) {
+    if (name === undefined || email === undefined || password === undefined || repassword === undefined) {
       window.alert("hooson bn");
       return;
+    }
+    if (password !== repassword) {
+      window.alert("nuuts ug taarahgui bn")
     }
     try {
       const response = await fetch("http://localhost:8080/",{
@@ -23,9 +28,11 @@ export default function SignUp (){
           Accept: "application/json, text/plain, */*",
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({name, email, password}),
+        body: JSON.stringify({name, email, password, id: uuidv4()}),
       });
+      console.log(body);
       const res = await response.json();
+      console.log(res);
     } catch (err) {
       console.error(err)
     }
@@ -33,9 +40,7 @@ export default function SignUp (){
     return (<div className="flex items-center justify-center bg-white h-[100vh] w-[50%]">      
     <style>{'body { background-color: #0166FF; height:100%;}'}</style>
     <div className="flex flex-col gap-[40px] items-center">
-      <div>
-        {geld}
-      </div>
+      {geld}
       <div className="flex flex-col items-center gap-3">
         <div className="text-4xl font-bold">Create Geld account</div>
         <div className="text-2xl text-gray-600">Sign up below to create your Wallet account</div>
@@ -44,7 +49,7 @@ export default function SignUp (){
         <input onChange={(event) => setName(event.target.value)} className="border-[1px] border-gray-300 bg-gray-100 rounded w-[450px] h-[50px] pl-3" type="text" placeholder="Name" />
         <input onChange={(event) => setEmail(event.target.value)} className="border-[1px] border-gray-300 bg-gray-100 rounded w-[450px] h-[50px] pl-3" type="email" placeholder="Email" />
         <input onChange={(event) => setPassword(event.target.value)} className="border-[1px] border-gray-300 bg-gray-100 rounded w-[450px] h-[50px] pl-3" type="password" placeholder="Password" />
-        <input className="border-[1px] border-gray-300 bg-gray-100 rounded w-[450px] h-[50px] pl-3" type="password" placeholder="Re-Password" />
+        <input onChange={(event) => setRepassword(event.target.value)} className="border-[1px] border-gray-300 bg-gray-100 rounded w-[450px] h-[50px] pl-3" type="password" placeholder="Re-Password" />
         <button onClick={createData} className=" bg-[#0166FF] text-white rounded-2xl w-[450px] h-[50px] text-xl">Sign up</button>
       </div>
       <div className="flex gap-3 text-xl">
